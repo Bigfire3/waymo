325d4a9529a6777ff73591822f17dd64516fe7a7
smn-hrtzsch <simon@hoertzsch.de>
2025-04-23 19:29:26 +0200

    rm all unnecessary files from documentation branch

--- run_waymo.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd ~/ros2_ws || { echo "Fehler: ros2_ws-Verzeichnis nicht gefunden."; exit 1; }
-colcon build --packages-select waymo
-source /opt/ros/humble/setup.bash
-source /home/fabian/ros2_ws/install/local_setup.bash
-
-ros2 launch waymo waymo_launch.py
-
